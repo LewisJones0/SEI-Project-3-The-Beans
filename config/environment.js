a2213ab6b1f0427631621c5804123f2142283f46
Lewis Jones <lewisjones0000@gmail.com>
2021-01-21 20:36:08 +0000

    Prep for Atlas
    Prep for Atlas

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,6 @@
-const dbURI = 'mongodb://localhost/thebeans'
-const port = 4001
-const secret = 'top secret secret'
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://user123:user@cluster0.qlsht.mongodb.net/the-beans?retryWrites=true&w=majority'
+const port = process.env.PORT || 4000
+const secret = process.env.SECRET || 'shhhhitsasecret'
 
 module.exports = {
   dbURI,
