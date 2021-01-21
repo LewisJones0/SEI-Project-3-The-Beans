69ffffe29dba8112068c5d58ebecd71faf70ed54
Lewis Jones <lewisjones0000@gmail.com>
2021-01-21 23:50:29 +0000

    SchemaChange to fit Atlas

--- models/beans.js
+++ models/beans.js
@@ -9,7 +9,7 @@ const commentSchema = new mongoose.Schema({
 })
 
 const beanSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   image: { type: String, required: true },
   price: [{ type: Number, required: true }],
   weight: [{ type: Number, required: true }],
--- models/roaster.js
+++ models/roaster.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const roasterSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
+  name: { type: String, required: true },
   logo: { type: String, required: true },
   bio: { type: String, required: true },
   hasCoffeeShop: { type: Boolean, required: true },
