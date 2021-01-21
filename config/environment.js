c89a332851779e05a899319c8aaee609071cd9c3
Lewis Jones <lewisjones0000@gmail.com>
2021-01-21 22:07:43 +0000

    AtlasUpdate4
    AtlasUpdate4

--- config/environment.js
+++ config/environment.js
@@ -1,4 +1,4 @@
-const dbURI = process.env.MONGODB_URI || 'mongodb+srv://user123:user@cluster0.qlsht.mongodb.net/the-beans?retryWrites=true&w=majority'
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://user123:user@cluster0.qlsht.mongodb.net/thebeans?retryWrites=true&w=majority'
 const port = process.env.PORT || 4000
 const secret = process.env.SECRET || 'shhhhitsasecret'
 
